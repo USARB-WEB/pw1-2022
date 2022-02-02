2f92eb5800a8b9c9c442309e2ac034b3de000082
sergiuchilat <chilatsergiu@gmail.com>
2022-02-02 11:24:25 +0200

    4. Loops

---
+++ examples/4. Loops/app.js
@@ -0,0 +1,20 @@
+console.log("FOR");
+for (let index = 1; index <= 5; index++) {
+    console.log(index);
+}
+
+console.log("While");
+index = 1;
+while (index <= 5) {
+    console.log(index);
+    index++;
+}
+
+console.log("DO While");
+index = 1;
+do{
+    console.log(index);
+    index++;
+}while (index <= 5);
+
+
