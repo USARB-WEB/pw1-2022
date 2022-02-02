15ffb4b9706f4954526d6171b72619be677526e2
sergiuchilat <chilatsergiu@gmail.com>
2022-02-02 11:04:37 +0200

    2. JS Syntax

---
+++ examples/2. JS Syntax/app.js
@@ -0,0 +1,51 @@
+console.log("Hello. I am a JS console.log");
+
+let a = 7;
+const b = 3;
+
+console.log(a);
+console.log(b);
+
+a = 11;
+console.log(a);
+
+// b = 5; - will generate an error, because const can't be modified
+console.log(b);
+
+/*
+multiline 
+comment
+*/
+
+let sum = a + b;
+console.log("Sum(a+b) =",  sum);
+
+/*
+Arithmetical operators
+    +
+    -
+    *
+    /
+*/
+console.log("Arithmetical operators");
+let x = 1;
+let y = 2;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+x = "1";
+y = "a";
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+x = "1";
+y = "2";
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
