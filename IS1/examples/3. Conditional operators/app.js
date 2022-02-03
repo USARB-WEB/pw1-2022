71cc66f15c19bd5133e98c38a4ba4ee9b9b2c756
sergiuchilat <chilatsergiu@gmail.com>
2022-02-03 09:05:01 +0200

    3. Conditional operators

---
+++ IS1/examples/3. Conditional operators/app.js
@@ -0,0 +1,51 @@
+/*
+alert("Hello. My name is Java Script");
+const userName = prompt("What is your name?")
+alert(`Hello ${userName}`);
+const likeJS = confirm("Do you like JS");
+if(likeJS){
+    alert("Great! Go to frontend dev");
+} else {
+    alert("You can try backend");
+}
+*/
+
+const digit = Number(prompt("Input a digit"));
+
+switch (digit) {
+    case 0:
+        alert("Zero");
+        break;
+    case 1:
+        alert("One");
+        break;
+    case 2:
+        alert("Two");
+        break;
+    case 3:
+        alert("Three");
+        break;
+    case 4:
+        alert("Four");
+        break;
+    case 5:
+        alert("Five");
+        break;
+    case 6:
+        alert("Six");
+        break;
+    case 7:
+        alert("Seven");
+        break;
+    case 8:
+        alert("Eight");
+        break;
+    case 9:
+        alert("Nine");
+        break;
+    case 0:
+        alert("Ten");
+        break;
+    default:
+        alert("This is not a digit");
+}
