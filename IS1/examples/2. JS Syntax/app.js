5350ffa4777e101e609033b363a81a868b32e46e
sergiuchilat <chilatsergiu@gmail.com>
2022-02-03 08:50:42 +0200

    2. JS Syntax

---
+++ IS1/examples/2. JS Syntax/app.js
@@ -0,0 +1,44 @@
+console.log("Hello. I am a JS console");
+let a = 4;
+const b = 7;
+console.log(a);
+console.log(b);
+
+a = 8;
+console.log(a);
+//b = 3;
+//console.log(b);
+
+
+
+/*
+    Arithmetical operators
+  +
+  -
+  *
+  /
+*/
+let x = 1;
+let y = 2;
+console.log("Arithmetical operators");
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+x = "1";
+y = "2";
+console.log(x + y);//concatenate
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+x = '1';
+y = 'a';
+console.log(x + y);//concatenate
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+const sum = a + b;
+console.log(`${a} + ${b} = ${sum}`);
