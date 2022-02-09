107ecd79d0a191873e04167e91ae2d81668d0587
sergiuchilat <chilatsergiu@gmail.com>
2022-02-09 12:40:26 +0200

    8. Object

---
+++ IS2_MI/examples/8. Object/app.js
@@ -0,0 +1,74 @@
+const personArray = [
+    "Ion",
+    "Ciobanu",
+    33,
+    "+3731992345",
+    9.99,
+    "Chisinau",
+];
+
+console.log(personArray);
+console.log(personArray[1]);
+
+
+const personObject = {
+    name: "Ion",
+    surname: "Ciobanu",
+    age: 33,
+    phone: "+3731992345",
+    averageMark: 9.99,
+    city: "Chisinau"
+};
+console.log(personObject.age);
+console.log(personObject);
+
+
+const person = {
+    name: "Ion",
+    surname: "Ciobanu",
+    age: 33,
+    contacts: {
+        phone: "+3731992345",
+        email: "ion.creanga@usarb.md"
+    },
+    averageMark: 9.99,
+    city: "Chisinau"
+};
+
+console.log(person.contacts.phone);
+
+console.log(person);
+
+const employees = [
+    {
+        name: "Ion",
+        salary: 1000
+    },
+    {
+        name: "Ana",
+        salary: 4000
+    },
+    {
+        name: "Mihai",
+        salary: 3000
+    },
+    {
+        name: "Elena",
+        salary: 5000
+    }
+];
+
+console.log(employees);
+
+const salarySum = employees.reduce((sum, employee) => sum + Number(employee.salary), 0);
+console.log(salarySum);
+
+
+console.log(employees.filter(employee => employee.salary >= 4000));
+employees.splice(0, employees.length, employees.map(employee => {
+    return {
+        name: employee.name,
+        salary: employee.salary + 100
+    }
+}))
+console.log(employees);
